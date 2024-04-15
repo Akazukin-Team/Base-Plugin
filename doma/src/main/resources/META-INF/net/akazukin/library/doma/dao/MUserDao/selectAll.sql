710ed49e5082a15822a4390707859b54743a0bad
Currypan1229 <abcqqqwpvp@gmail.com>
2024-04-16 01:17:01 +0900

    SQLの可読性の向上

--- doma/src/main/resources/META-INF/net/akazukin/library/doma/dao/MUserDao/selectAll.sql
+++ doma/src/main/resources/META-INF/net/akazukin/library/doma/dao/MUserDao/selectAll.sql
@@ -1,2 +1,4 @@
-select /*%expand*/*
-from M_USER
+select
+    /*%expand*/*
+from
+    M_USER
--- doma/src/main/resources/META-INF/net/akazukin/library/doma/dao/MUserDao/selectByPlayer.sql
+++ doma/src/main/resources/META-INF/net/akazukin/library/doma/dao/MUserDao/selectByPlayer.sql
@@ -1,3 +1,6 @@
-select /*%expand*/*
-from M_USER
-where PLAYER_UUID = /* player */'00aaa0a0-000a-000a-00aa-00a0000a0a00'
+select
+    /*%expand*/*
+from
+    M_USER
+where
+    PLAYER_UUID = /* player */'00aaa0a0-000a-000a-00aa-00a0000a0a00'
