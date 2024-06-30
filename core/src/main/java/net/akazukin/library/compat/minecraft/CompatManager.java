693239972b2391047c36c9620a3c8a3fb9215a1d
Currypan1229 <abcqqqwpvp@gmail.com>
2024-06-30 22:41:47 +0900

    CompatManagerの修正

--- core/src/main/java/net/akazukin/library/compat/minecraft/CompatManager.java
+++ core/src/main/java/net/akazukin/library/compat/minecraft/CompatManager.java
@@ -4,12 +4,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import lombok.Getter;
 import net.akazukin.library.LibraryPlugin;
+import org.bukkit.Bukkit;
 
 @Getter
 public class CompatManager {
     public static Compat initCompat() {
-        final String version = LibraryPlugin.getPlugin().getServer().getClass().getPackage().getName().split("\\.")[3];
-        return getCompat("net.akazukin.library.compat.minecraft.compats.Compat_" + version);
+        return getCompat("net.akazukin.library.compat.minecraft.compats.Compat_" + getMappingVersion());
     }
 
     public static Compat getCompat(final String clazzName) {
@@ -24,6 +24,58 @@ public class CompatManager {
             LibraryPlugin.getLogManager().log(Level.SEVERE, e.getMessage(), e);
         }
         return null;
+    }
+
+    public static String getMappingVersion() {
+        switch (Bukkit.getServer().getBukkitVersion().split("-")[0]) {
+            case "1.21": {
+                return "v_1_21_R1";
+            }
+
+            case "1.20.5":
+            case "1.20.6": {
+                return "v_1_20_R4";
+            }
+
+            case "1.20.3":
+            case "1.20.4": {
+                return "v_1_20_R3";
+            }
+
+            case "1.20.2": {
+                return "v_1_20_R2";
+            }
+
+            case "1.20":
+            case "1.20.1": {
+                return "v_1_20_R1";
+            }
+
+            case "1.19.4": {
+                return "v_1_19_R3";
+            }
+
+            case "1.19.3": {
+                return "v_1_19_R2";
+            }
+
+            case "1.19":
+            case "1.19.1":
+            case "1.19.2": {
+                return "v_1_19_R1";
+            }
+
+            case "1.18":
+            case "1.18.1":
+            case "1.18.2": {
+                return "v_1_18_R1";
+            }
+
+
+            default: {
+                return null;
+            }
+        }
     }
 
     public static Compat getCompat(final Class<? extends Compat> clazz) {
