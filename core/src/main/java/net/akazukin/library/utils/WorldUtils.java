a29804d715d660afe6701982e8fd8a3e4a346c55
Currypan1229 <abcqqqwpvp@gmail.com>
2024-07-21 06:40:44 +0900

    WorldUtilsのmethod追加と修正

--- core/src/main/java/net/akazukin/library/utils/WorldUtils.java
+++ core/src/main/java/net/akazukin/library/utils/WorldUtils.java
@@ -1,6 +1,7 @@
 package net.akazukin.library.utils;
 
 import java.io.File;
+import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 import net.akazukin.library.compat.worldguard.WorldGuardCompat;
 import net.akazukin.library.world.WorldData;
@@ -9,24 +10,27 @@ import org.bukkit.World;
 import org.bukkit.WorldCreator;
 
 public class WorldUtils {
-    public static World loadWorld(@Nonnull final WorldData worldData) {
+    public static World getOrLoadWorld(@Nonnull final WorldData worldData) {
+        return getOrLoadWorld(worldData, () -> Bukkit.createWorld(WorldCreator.name(worldData.getName())));
+    }
+
+    public static World getOrLoadWorld(@Nonnull final WorldData worldData, @Nonnull final Supplier<World> creator) {
         if (!worldData.isValid())
             throw new IllegalArgumentException("World data is invalid");
 
         final World w = getWorld(worldData);
         if (w != null) return w;
 
-        if (worldData.getName() != null && new File(Bukkit.getWorldContainer(), worldData.getName()).exists()) {
-            final World w2 = Bukkit.createWorld(WorldCreator.name(worldData.getName()));
-            return w2;
-        }
-
-        return null;
+        return creator.get();
     }
 
     public static World getWorld(@Nonnull final WorldData worldData) {
         return worldData.getUid() != null ? Bukkit.getWorld(worldData.getUid()) :
                 (worldData.getName() != null ? Bukkit.getWorld(worldData.getName()) : null);
+    }
+
+    public static boolean isWorldLoaded(@Nonnull final WorldData worldData) {
+        return getWorld(worldData) != null;
     }
 
     public static boolean deleteWorld(@Nonnull final World world) {
