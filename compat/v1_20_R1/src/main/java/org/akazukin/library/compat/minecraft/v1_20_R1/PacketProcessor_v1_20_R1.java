6de68fd2c04dfbd9c07a8e026110bb003e1a2d8f
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2025-01-04 22:37:40 +0900

    Add supported pkts for PacketProcessor

--- compat/v1_20_R1/src/main/java/org/akazukin/library/compat/minecraft/v1_20_R1/PacketProcessor_v1_20_R1.java
+++ compat/v1_20_R1/src/main/java/org/akazukin/library/compat/minecraft/v1_20_R1/PacketProcessor_v1_20_R1.java
@@ -1,28 +1,64 @@
 package org.akazukin.library.compat.minecraft.v1_20_R1;
 
+import it.unimi.dsi.fastutil.shorts.ShortArraySet;
 import lombok.AllArgsConstructor;
+import net.minecraft.core.SectionPosition;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.PacketListenerPlayOut;
+import net.minecraft.network.protocol.game.ClientboundInitializeBorderPacket;
 import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
 import net.minecraft.network.protocol.game.PacketPlayOutBlockChange;
+import net.minecraft.network.protocol.game.PacketPlayOutMultiBlockChange;
 import net.minecraft.network.protocol.game.PacketPlayOutOpenSignEditor;
+import net.minecraft.world.level.block.state.IBlockData;
 import org.akazukin.library.compat.minecraft.compats.Compat_v1_20_R1;
 import org.akazukin.library.compat.minecraft.data.PacketProcessor;
+import org.akazukin.library.compat.minecraft.data.packets.CInitializeBorderPacket;
 import org.akazukin.library.compat.minecraft.data.packets.COpenSignEditorPacket;
 import org.akazukin.library.compat.minecraft.data.packets.SBlockChangePacket;
+import org.akazukin.library.compat.minecraft.data.packets.SMultiBlockChangePacket;
 import org.akazukin.library.compat.minecraft.data.packets.SUpdateSignPacket;
+import org.akazukin.library.utils.ArrayUtils;
+import org.bukkit.craftbukkit.v1_20_R1.CraftWorldBorder;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 public class PacketProcessor_v1_20_R1 implements PacketProcessor<Packet<?>> {
     private final Compat_v1_20_R1 compat;
 
     @Override
-    public Packet<PacketListenerPlayOut> processWrapper(final org.akazukin.library.compat.minecraft.data.packets.Packet packet) {
+    public Packet<?> processWrapper(final org.akazukin.library.compat.minecraft.data.packets.Packet packet) {
         //SPacket only supports
         if (packet instanceof COpenSignEditorPacket) {
             return new PacketPlayOutOpenSignEditor(
                     this.compat.getNMSBlockPos(((COpenSignEditorPacket) packet).getWrappedBlockPos()),
                     ((COpenSignEditorPacket) packet).isFrontText()
+            );
+        } else if (packet instanceof SUpdateSignPacket) {
+            return new PacketPlayInUpdateSign(
+                    this.compat.getNMSBlockPos(((SUpdateSignPacket) packet).getPosition()),
+                    ((SUpdateSignPacket) packet).isOutlined(),
+                    ArrayUtils.getIndex(((SUpdateSignPacket) packet).getLines(), 0),
+                    ArrayUtils.getIndex(((SUpdateSignPacket) packet).getLines(), 1),
+                    ArrayUtils.getIndex(((SUpdateSignPacket) packet).getLines(), 2),
+                    ArrayUtils.getIndex(((SUpdateSignPacket) packet).getLines(), 3)
+            );
+        } else if (packet instanceof CInitializeBorderPacket) {
+            return new ClientboundInitializeBorderPacket(
+                    ((CraftWorldBorder) ((CInitializeBorderPacket) packet).getWorldBorder())
+                            .getHandle()
+            );
+        } else if (packet instanceof SMultiBlockChangePacket) {
+            return new PacketPlayOutMultiBlockChange(
+                    SectionPosition.a(
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getX(),
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getY(),
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getZ()),
+                    new ShortArraySet(Arrays.stream(((SMultiBlockChangePacket) packet).getBlockInfo()).map(b ->
+                            (short) ((b.getPos().getX() & 0xF) << 8 | (b.getPos().getZ() & 0xF) << 4 | b.getPos().getY() & 0xF)).toList()),
+                    Arrays.stream(((SMultiBlockChangePacket) packet).getBlockInfo())
+                            .map(b -> (IBlockData) b.getBlockData().getBlockData())
+                            .toArray(IBlockData[]::new)
             );
         } else if (packet instanceof SBlockChangePacket) {
             return new PacketPlayOutBlockChange(
--- compat/v1_20_R2/src/main/java/org/akazukin/library/compat/minecraft/v1_20_R2/PacketProcessor_v1_20_R2.java
+++ compat/v1_20_R2/src/main/java/org/akazukin/library/compat/minecraft/v1_20_R2/PacketProcessor_v1_20_R2.java
@@ -1,19 +1,26 @@
 package org.akazukin.library.compat.minecraft.v1_20_R2;
 
+import it.unimi.dsi.fastutil.shorts.ShortArraySet;
 import lombok.AllArgsConstructor;
+import net.minecraft.core.SectionPosition;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundInitializeBorderPacket;
 import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
 import net.minecraft.network.protocol.game.PacketPlayOutBlockChange;
+import net.minecraft.network.protocol.game.PacketPlayOutMultiBlockChange;
 import net.minecraft.network.protocol.game.PacketPlayOutOpenSignEditor;
+import net.minecraft.world.level.block.state.IBlockData;
 import org.akazukin.library.compat.minecraft.compats.Compat_v1_20_R2;
 import org.akazukin.library.compat.minecraft.data.PacketProcessor;
 import org.akazukin.library.compat.minecraft.data.packets.CInitializeBorderPacket;
 import org.akazukin.library.compat.minecraft.data.packets.COpenSignEditorPacket;
 import org.akazukin.library.compat.minecraft.data.packets.SBlockChangePacket;
+import org.akazukin.library.compat.minecraft.data.packets.SMultiBlockChangePacket;
 import org.akazukin.library.compat.minecraft.data.packets.SUpdateSignPacket;
 import org.akazukin.library.utils.ArrayUtils;
 import org.bukkit.craftbukkit.v1_20_R2.CraftWorldBorder;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 public class PacketProcessor_v1_20_R2 implements PacketProcessor<Packet<?>> {
@@ -40,6 +47,18 @@ public class PacketProcessor_v1_20_R2 implements PacketProcessor<Packet<?>> {
             return new ClientboundInitializeBorderPacket(
                     ((CraftWorldBorder) ((CInitializeBorderPacket) packet).getWorldBorder())
                             .getHandle()
+            );
+        } else if (packet instanceof SMultiBlockChangePacket) {
+            return new PacketPlayOutMultiBlockChange(
+                    SectionPosition.a(
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getX(),
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getY(),
+                            ((SMultiBlockChangePacket) packet).getSectionPos().getZ()),
+                    new ShortArraySet(Arrays.stream(((SMultiBlockChangePacket) packet).getBlockInfo()).map(b ->
+                            (short) ((b.getPos().getX() & 0xF) << 8 | (b.getPos().getZ() & 0xF) << 4 | b.getPos().getY() & 0xF)).toList()),
+                    Arrays.stream(((SMultiBlockChangePacket) packet).getBlockInfo())
+                            .map(b -> (IBlockData) b.getBlockData().getBlockData())
+                            .toArray(IBlockData[]::new)
             );
         } else if (packet instanceof SBlockChangePacket) {
             return new PacketPlayOutBlockChange(
